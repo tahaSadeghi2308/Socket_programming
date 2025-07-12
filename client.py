1eb2c96e22870da9bb2881c3446b9a7dcfb0ad79
Taha <maralahmadi2308@gmail.com>
2025-07-12 11:56:00 +0330

    Fix a bug in closing connection

--- client.py
+++ client.py
@@ -6,7 +6,10 @@ cli.connect(("ip of server" , 4225))
 
 while True:
     msg = input("message: ")
+    if msg == "e": break
     cli.send(msg.encode("UTF-8"))
     res = cli.recv(2048).decode("UTF-8")
     print(f"Response {res}")
+
+cli.close()
 
